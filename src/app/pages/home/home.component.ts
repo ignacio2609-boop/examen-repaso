6da2eada7c410e52bb8bf348543d64ead29bdf38
Ignacio Bonilla <ignacio.bonilla@live.u-tad.com>
2024-06-23 20:37:09 +0200

    Examen completo y funcional

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,47 +21,31 @@ import { Alumno } from './alumno.model';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
   disable: any;
+  disabledButtons: { [key: string]: boolean } = {};
   alumnos: Alumno[] = [
     { nombre: "Juan", apellidos: "Pérez Gómez", dni: "12345678A", telefono: "612345678"},
     { nombre: "María", apellidos: "Rodríguez Sánchez", dni: "87654321X", telefono: "678901234" },
     { nombre: "Carlos", apellidos: "Fernández Díaz", dni: "11223344A", telefono: "712345678" }
   ];
-
-  idBotones: string[] =[
-    "12345678A",
-    "87654321X",
-    "11223344A"
-  ]
-
-
+// Función añadir alumno
 addAlumno(alumno: any) {
   console.log("Funciona evento!!", alumno, alumno.dni);
   this.alumnos.push(alumno);
-  this.idBotones.push(alumno.dni)
   }
-
+// Función eliminar alumno
   deleteAlumno(alumno: any) {
-    console.log("Funciona boton!!", alumno, alumno.dni);
+    console.log("Funciona delete!!", alumno, alumno.dni);
     for (var i = 0; i < this.alumnos.length; i++) {
       if (alumno.dni == this.alumnos[i].dni) {
         this.alumnos.splice(i, 1);
-        break
+        i = this.alumnos.length;
       }
     }
   }
-
-  presente(alumno: any) {
-    let id: string
-    console.log("Funciona boton!!");
-      for (var i = 0; i < this.alumnos.length; i++) {
-        id = "Alumno " + this.alumnos[i].dni;
-        console.log(id);
-        if (("Alumno " + alumno.dni) == document.getElementById(id)?.id) {
-          console.log(document.getElementById(id)?.id);
-          this.disable = document.getElementById(id)?.id;
-          console.log(this.disable)
-        }
-      }
+// Función para marcar como presente
+  presente(alumno: Alumno) {
+    this.disabledButtons[alumno.dni] = true;
     }
 }
